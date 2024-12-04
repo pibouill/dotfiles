a3436396e72e475e93c981fcdeef7df40cdd45b8
pibouill <pibouill@student.42prague.com>
2024-12-04 14:18:51 +0100

    (v) saving stuff

--- nvim/lua/plugins/no-neckpain.lua
+++ nvim/lua/plugins/no-neckpain.lua
@@ -2,7 +2,9 @@ return {
   {
     "shortcuts/no-neck-pain.nvim",
     cmd = "NoNeckPain",
-    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>"} },
-    opts = {},
+    keys = {
+		{ "<leader>nn", "<cmd>NoNeckPain<cr>"},
+		{ "<leader>nN", "<cmd>NoNeckPainResize 120<cr>"},
+	},
   },
 }
