a25b9ae4e3d05d81e77db4d755102a8b74419912
pibouill <pibouill@student.42prague.com>
2024-12-17 10:27:35 +0100

    (v) copilot

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,19 +19,35 @@ return {
 		local cmp = require('cmp')
 		local lspkind = require('lspkind')
 
+		-- Function to check if there are words before the cursor
+		local has_words_before = function()
+		  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+		  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+		end
+
 		cmp.setup({
 		  sources = {
 			{ name = 'nvim_lsp' },
 			{ name = 'copilot', group_index = 2 },
+			{ name = 'path', group_index = 2 },
+			{ name = 'luasnip', group_index = 2 },
 		  },
 		  mapping = cmp.mapping.preset.insert({
 			['<C-Space>'] = cmp.mapping.complete(),
 			['<C-u>'] = cmp.mapping.scroll_docs(-4),
 			['<C-d>'] = cmp.mapping.scroll_docs(4),
+			["<Tab>"] = vim.schedule_wrap(function(fallback)
+			  if cmp.visible() and has_words_before() then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			  else
+				fallback()
+			  end
+			end),
 		  }),
 		  snippet = {
 			expand = function(args)
-			  vim.fn["vsnip#anonymous"](args.body) -- Replace with your snippet engine. Example: vsnip.
+			  vim.fn["vsnip#anonymous"](args.body) -- Replace with your snippet engine, e.g., luasnip or vsnip.
 			end,
 		  },
 		  formatting = {
@@ -39,9 +55,28 @@ return {
 			  mode = 'symbol',
 			  maxwidth = 50, -- Maximum width of menu items
 			  ellipsis_char = '...', -- Truncated text will be replaced with ellipsis
+			  symbol_map = { Copilot = "" },
 			}),
 			expandable_indicator = false,
 			fields = { cmp.ItemField.Abbr, cmp.ItemField.Menu },
+		  },
+		  sorting = {
+			priority_weight = 2,
+			comparators = {
+			  require("copilot_cmp.comparators").prioritize,
+
+			  -- Below is the default comparitor list and order for nvim-cmp
+			  cmp.config.compare.offset,
+			  -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+			  cmp.config.compare.exact,
+			  cmp.config.compare.score,
+			  cmp.config.compare.recently_used,
+			  cmp.config.compare.locality,
+			  cmp.config.compare.kind,
+			  cmp.config.compare.sort_text,
+			  cmp.config.compare.length,
+			  cmp.config.compare.order,
+			},
 		  },
 		})
 	  end,
