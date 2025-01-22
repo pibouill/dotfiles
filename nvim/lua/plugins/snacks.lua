3a6a3daa0369c515c57fdbacaa408a7363d3dd89
pibouill <pibouill@student.42prague.com>
2025-01-22 15:48:17 +0100

    (v) copilot chat

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -30,7 +30,7 @@ return {
     words = { enabled = true },
     styles = {
       notification = {
-        -- wo = { wrap = true } -- Wrap notifications
+		  relative = "editor",
       }
     }
   },
@@ -48,8 +48,6 @@ return {
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
 
@@ -57,6 +55,7 @@ return {
       "<leader>N",
       desc = "Neovim News",
       function()
+		  local Snacks = require("snacks")
         Snacks.win({
           file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
           width = 0.6,
@@ -87,25 +86,15 @@ return {
 
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-
         Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-
         Snacks.toggle.diagnostics():map("<leader>ud")
-
         Snacks.toggle.line_number():map("<leader>ul")
-
         Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
-
         Snacks.toggle.treesitter():map("<leader>uT")
-
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-
         Snacks.toggle.inlay_hints():map("<leader>uh")
-
         Snacks.toggle.indent():map("<leader>ug")
-
+		-- Dimming
         Snacks.toggle.dim():map("<leader>uD")
       end,
     })
