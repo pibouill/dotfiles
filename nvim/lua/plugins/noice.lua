30bbab4faf3d00c00598f449f35e9f7007e9b6b7
pibouill <pibouill@student.42prague.com>
2025-01-27 17:52:58 +0100

    (v) restructuring noice.lua

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -11,41 +11,41 @@
 -- ************************************************************************** --
 
 return {
-	{
-	  "folke/noice.nvim",
-	  opts = {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
 		lsp = {
-		  override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
-		  },
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
 		},
 		routes = {
-		  {
-			filter = {
-			  event = "msg_show",
-			  any = {
-				{ find = "%d+L, %d+B" },
-				{ find = "; after #%d+" },
-				{ find = "; before #%d+" },
-			  },
+			{
+				filter = {
+					event = "msg_show",
+					any = {
+						{ find = "%d+L, %d+B" },
+						{ find = "; after #%d+" },
+						{ find = "; before #%d+" },
+					},
+				},
+				view = "mini",
 			},
-			view = "mini",
-		  },
 		},
 		presets = {
-		  bottom_search = true,
-		  command_palette = false,
-		  long_message_to_split = true,
-		  lsp_doc_border = true,
+			bottom_search = true,
+			command_palette = false,
+			long_message_to_split = true,
+			lsp_doc_border = true,
 		},
-	  },
-	  dependencies = {
-		  "MunifTanjim/nui.nvim",
-	  },
-		-- stylua: ignore
-	  keys = {
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	-- stylua: ignore
+	keys = {
 		{ "<leader>sn", "", desc = "+noice"},
 		{ "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
 		{ "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
@@ -55,15 +55,14 @@ return {
 		{ "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
 		{ "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
 		{ "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
-	  },
-	  config = function(_, opts)
+	},
+	config = function(_, opts)
 		-- HACK: noice shows messages from before it was enabled,
 		-- but this is not ideal when Lazy is installing plugins,
 		-- so clear the messages in this case.
 		if vim.o.filetype == "lazy" then
-		  vim.cmd([[messages clear]])
+			vim.cmd([[messages clear]])
 		end
 		require("noice").setup(opts)
-	  end,
-  },
+	end,
 }
