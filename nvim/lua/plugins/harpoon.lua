5abb8b8d93902a4ddc45ce2aff9d15b487bb847d
pibouill <pibouill@student.42prague.com>
2025-03-04 14:05:02 +0100

    (v) have to delete .../lazy/ to fix harpoon?

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -44,7 +44,7 @@ return {
 				desc = "Harpoon Quick Menu",
 			},
 		}
-		local is_macos = vim.uv.os_uname().sysname == "Darwin"
+		local is_macos = vim.loop.os_uname().sysname == "Darwin"
 
 		if is_macos then
 			-- OSX-specific mappings
@@ -71,30 +71,3 @@ return {
 		return keys
 	end,
 }
--- return {
---     "ThePrimeagen/harpoon",
---     branch = "harpoon2",
---     dependencies = {
---         "nvim-lua/plenary.nvim"
---     },
---     config = function()
---         local harpoon = require("harpoon")
---         harpoon:setup({
---             settings = {
---                 save_on_toggle = true,
---                 sync_on_ui_close = true,
---             },
---         })
---         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
---         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
---         vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
---         vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
---         vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
---         vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
---
---         -- Toggle previous & next buffers stored within Harpoon list
--- 		-- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- 		-- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
---     end,
--- }
