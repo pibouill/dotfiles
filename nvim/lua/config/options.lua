0bbab37550054cb01d57389e12fe1431c5ce498d
pibouill <pibouill@student.42prague.com>
2025-07-24 20:05:11 +0200

    adding clang-format and (v)generate

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -56,6 +56,14 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.tabstop = 4
     end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cpp",
+    callback = function()
+        vim.opt_local.expandtab = true
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.tabstop = 4
+    end,
+})
 
 -- Cursor styling
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
