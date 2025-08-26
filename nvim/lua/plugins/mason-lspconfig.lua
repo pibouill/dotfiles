3ef1c47c7c1ca77a9f562a6b2009119390a9c17c
pibouill <pibouill@student.42prague.com>
2025-08-26 21:44:46 +0200

    (v)update(vz)make re alias

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -11,23 +11,23 @@
 -- ************************************************************************** --
 
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
-      },
-    },
-    "mason-org/mason-lspconfig.nvim",
-    "williamboman/mason.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
-  },
-  config = function()
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		"mason-org/mason-lspconfig.nvim",
+		"williamboman/mason.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+	},
+	config = function()
 		local mason_lspconfig = require("mason-lspconfig")
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -61,27 +61,13 @@ return {
 					"clangd",
 					"--offset-encoding=utf-16",
 				},
+				filetypes = { "c", "cpp", "h", "hpp" },
+			},
+			bashls = {
+				filetypes = { "sh", "zsh", "bash", "zshrc", "bashrc" },
 			},
 		}
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(ev)
-				local bufnr = ev.buf
-				vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-				-- Key mappings for LSP features
-				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "LSP: Go to definition" })
-				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = "LSP: Go to declaration" })
-				vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = "LSP: Go to references" })
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover" })
-				vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP: Rename symbol" })
-				vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = "LSP: Code action" })
-				vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { buffer = bufnr, desc = "LSP: Show diagnostics" })
-				vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr, desc = "LSP: Previous diagnostic" })
-				vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr, desc = "LSP: Next diagnostic" })
-			end,
-		})
-
-		-- Setup all servers with common config + server-specific settings
 		mason_lspconfig.setup({
 			ensure_installed = {
 				"lua_ls",
@@ -102,9 +88,58 @@ return {
 					if server_settings[server_name] then
 						opts = vim.tbl_deep_extend("force", opts, server_settings[server_name])
 					end
+					if server_name == "clangd" then
+						opts.filetypes = { "c", "cpp", "h", "hpp" }
+					end
+					vim.notify("Setting up LSP server: " .. server_name, vim.log.levels.INFO)
 					lspconfig[server_name].setup(opts)
 				end,
 			},
+		})
+		vim.filetype.add({
+			extension = {
+				zsh = "zsh",
+			},
+			filename = {
+				[".zshrc"] = "zsh",
+				["zshrc"] = "zsh",
+			},
+			pattern = {
+				[".*zshrc.*"] = "zsh",
+			},
+		})
+
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = { "*zshrc", "*.zsh", ".zshrc", "zshrc" },
+			callback = function()
+				vim.bo.filetype = "zsh"
+			end,
+		})
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "zsh",
+			callback = function()
+				vim.defer_fn(function()
+					vim.cmd("LspStart bashls")
+				end, 100)
+			end,
+		})
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				local bufnr = ev.buf
+				vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+				-- Key mappings for LSP features
+				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "LSP: Go to definition" })
+				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = "LSP: Go to declaration" })
+				vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = "LSP: Go to references" })
+				vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover" })
+				vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP: Rename symbol" })
+				vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action,
+					{ buffer = bufnr, desc = "LSP: Code action" })
+				vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float,
+					{ buffer = bufnr, desc = "LSP: Show diagnostics" })
+				vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr, desc = "LSP: Previous diagnostic" })
+				vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr, desc = "LSP: Next diagnostic" })
+			end,
 		})
 	end,
 }
---
+++ nvim/lua/plugins/null_ls.lua
@@ -0,0 +1,37 @@
+-- ************************************************************************** --
+--                                                                            --
+--                                                        :::      ::::::::   --
+--   null_ls.lua                                        :+:      :+:    :+:   --
+--                                                    +:+ +:+         +:+     --
+--   By: pibouill <pibouill@student.42prague.com>   +#+  +:+       +#+        --
+--                                                +#+#+#+#+#+   +#+           --
+--   Created: 2025/08/25 18:44:34 by pibouill          #+#    #+#             --
+--   Updated: 2025/08/25 18:44:43 by pibouill         ###   ########.fr       --
+--                                                                            --
+-- ************************************************************************** --
+
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+
+    require("mason").setup({
+      ensure_installed = {
+        "checkmake",
+      }
+    })
+
+    null_ls.setup({
+      debug = false,
+      sources = {
+        null_ls.builtins.diagnostics.checkmake.with({
+          filetypes = { "make", "makefile" },
+        }),
+      },
+    })
+  end,
+}
