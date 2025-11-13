3e1a0c3dbec8ad37d07cf82139dc7c34e45059b3
pibouill <pibouill@student.42prague.com>
2025-11-13 20:36:06 +0100

    (v)update conform try and keep cursor placement

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -11,18 +11,20 @@
 -- ************************************************************************** --
 
 return {
-  "stevearc/conform.nvim",
-  -- dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cF",
-      function()
-        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
-    },
-  },
+	"stevearc/conform.nvim",
+	-- dependencies = { "mason.nvim" },
+	lazy = true,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cF",
+			function()
+				local pos = vim.api.nvim_win_get_cursor(0)
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+				vim.api.nvim_win_set_cursor(0, pos)
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
 }
