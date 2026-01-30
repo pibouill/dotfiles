15a59f4b40d6b9b91951ccb94d63b9c201cff9a3
pibouill <pibouill@student.42prague.com>
2026-01-30 13:49:18 +0100

    (v)fixing desc. window cmp. update clangd include paths

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -62,7 +62,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.expandtab = false
         vim.opt_local.shiftwidth = 4
         vim.opt_local.tabstop = 4
-		vim.opt_local.indentexpr=""
+		-- vim.opt_local.indentexpr=""
     end,
 })
 
