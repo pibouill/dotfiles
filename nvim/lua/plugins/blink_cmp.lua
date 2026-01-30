8377dcabd7fc2acec0a8b4d3146d4120e4a59d6c
pibouill <pibouill@student.42prague.com>
2026-01-30 15:36:12 +0100

    (v)fixed blink

---
+++ nvim/lua/plugins/blink_cmp.lua
@@ -0,0 +1,39 @@
+-- ************************************************************************** --
+--                                                                            --
+--                                                        :::      ::::::::   --
+--   blink_cmp.lua                                      :+:      :+:    :+:   --
+--                                                    +:+ +:+         +:+     --
+--   By: pibouill <pibouill@student.42prague.com>   +#+  +:+       +#+        --
+--                                                +#+#+#+#+#+   +#+           --
+--   Created: 2026/01/30 15:28:50 by pibouill          #+#    #+#             --
+--   Updated: 2026/01/30 15:30:47 by pibouill         ###   ########.fr       --
+--                                                                            --
+-- ************************************************************************** --
+
+return {
+	"saghen/blink.cmp",
+	build = "cargo build --release",
+	opts = {
+		signature = {
+			enabled = true,
+			trigger = {
+				enabled = true,
+				show_on_keyword = true,
+				show_on_insert = true,
+			},
+		},
+	},
+	window = {
+		border = "rounded",
+		treesitter_highlighting = true,
+		show_documentation = true,
+	},
+	config = function(_, opts)
+
+		require("blink.cmp").setup(opts)
+
+		vim.keymap.set("i", "<C-x>", function()
+			require("blink.cmp").show()
+		end, { noremap = true, silent = true })
+	end,
+}
