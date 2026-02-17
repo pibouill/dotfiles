bd3cda41a8d6b5ad4c01f11cafb68d13925c39bf
pibouill <pibouill@student.42prague.com>
2026-02-17 12:29:16 +0100

    (v)42 header updated fixed

--- nvim/lua/plugins/42_header.lua
+++ nvim/lua/plugins/42_header.lua
@@ -6,27 +6,37 @@
 --   By: pibouill <pibouill@student.42prague.com>   +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2024/12/06 09:44:14 by pibouill          #+#    #+#             --
---   Updated: 2024/12/06 09:45:50 by pibouill         ###   ########.fr       --
+--   Updated: 2026/02/17 12:00:39 by pibouill         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
 return {
-  "Diogo-ss/42-header.nvim",
-  cmd = { "Stdheader" },
-  keys = {
-    { "<leader>H", "<cmd>Stdheader<cr>", desc = "Insert/Update 42 Header" }
-  },
-  opts = {
-    default_map = false, -- Default mapping <F1> in normal mode.
-    auto_update = true, -- Update header when saving.
-    user = "pibouill",
-    mail = "pibouill@student.42prague.com",
-	git = {
-		enabled = true,
-		bin = "git",
-	}
-  },
-  config = function(_, opts)
-    require("42header").setup(opts)
-  end,
+	"Diogo-ss/42-header.nvim",
+	cmd = { "Stdheader" },
+	keys = {
+		{ "<leader>H", "<cmd>Stdheader<cr>", desc = "Insert/Update 42 Header" }
+	},
+	event = { "BufRead", "BufNewFile" },
+
+	opts = {
+		default_map = false, -- Default mapping <F1> in normal mode.
+		auto_update = false, -- Update header when saving.
+		user = "pibouill",
+		mail = "pibouill@student.42prague.com",
+		git = {
+			enabled = true,
+			bin = "git",
+		}
+	},
+
+	config = function(_, opts)
+		require("42header").setup(opts)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				if vim.bo.modified then
+					vim.cmd("Stdheader")
+				end
+			end,
+		})
+	end,
 }
